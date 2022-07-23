9143071d9fb83a55d9901bc9a422356bf80b635f
MateuszSajko <mateuszsajko@gmail.com>
2022-07-23 12:00:16 +0200

    Zmiana w klasie "TextFromUser" żeby można było wprowadzać imię, nazwisko oraz wiek użytkownika

--- src/TextFromUser.java
+++ src/TextFromUser.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class TextFromUser {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        System.out.println("Witaj " + userInput);
+        System.out.println("Wprowadz imie uzytkownika");
+        String imie = scanner.nextLine();
+        System.out.println("Wprowadz nazwisko uzytkownika");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Wprowadz wiek uzytkownika");
+        int wiek = scanner.nextInt();
+        System.out.println("Witaj " + imie + " "  + nazwisko + ". Masz " + wiek + " lat.");
 
     }
 }
