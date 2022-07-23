f3c56f34d4dc2e16e3130a5be0b521940e592c31
MateuszSajko <mateuszsajko@gmail.com>
2022-07-23 13:24:35 +0200

    pętla "Kotek" z imionami kotków

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,10 +1,11 @@
 public class ForLoop {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-            if (i==5){
-                System.out.println("Kotek");
-            }
+        String[] catBoard = new String[]{"Karmi", "Bubu", "Parker", "Simba", "Tofik"};
+        for (int i = 0; i < catBoard.length; i++){
+            System.out.println(catBoard[i]);
+
+
         }
     }
 }
+
